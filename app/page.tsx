7d69aae744263d82a75b34a21dab333ab9b30570
Dylan Lee <99694473+dev-dyl@users.noreply.github.com>
2025-05-15 00:45:13 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import Iframe from 'react-iframe';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <iframe src="https://discord.com/widget?id=1342452098528448653&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <Iframe 
+          src="https://discord.com/widget?id=1342452098528448653&theme=dark" 
+          width="350" 
+          height="500" 
+          allowtransparency="true" 
+          frameborder="0" 
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
