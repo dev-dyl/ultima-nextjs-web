7a9cfc47844360c019df09e2fa73f9fdf2e1f600
Dylan Lee <99694473+dev-dyl@users.noreply.github.com>
2025-05-16 15:11:33 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ export default function Home() {
         <iframe 
           src="https://discord.com/widget?id=1342452098528448653&theme=dark" 
           width="350" 
-          height="500" 
-          allowTransparency=true 
-          frameBorder="0" 
+          height="500"
           sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
         </iframe>
       </main>
